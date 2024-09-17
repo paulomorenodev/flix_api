07d51100d133a2f6c3134d66433bd0855c73b495
paulomorenodev <23208589@aluno.univesp.br>
2024-09-17 09:41:21 -0300

    detail_view_class_121

--- genres/views.py
+++ genres/views.py
@@ -1,10 +1,11 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from genres.models import Genre
 
 @csrf_exempt
-def genre_view(request):
+def genre_create_list_view(request):
     
     if request.method == 'GET':
         genres = Genre.objects.all()
@@ -15,7 +16,6 @@ def genre_view(request):
         #     {'id': genre.id, 'name': genre.name}
         # )
         return JsonResponse(data, safe=False)
-    
     elif request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         new_genre = Genre(name=data['name'])
@@ -24,3 +24,8 @@ def genre_view(request):
             {'id': new_genre.id, 'name': new_genre.name},
             status=201,
         )
+@csrf_exempt    
+def genre_detail_view(request, pk):
+    genre = Genre.objects.get(pk=pk)
+    data = {'id': genre.id, 'name': genre.name}
+    return JsonResponse(data)
