b40129ad3f39b56133e630699eaa857be1d9289e
paulomorenodev <23208589@aluno.univesp.br>
2024-09-17 15:18:44 -0300

    start_DRF_list_create_view_class_126

--- genres/views.py
+++ genres/views.py
@@ -3,27 +3,35 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from genres.models import Genre
+from rest_framework import generics
+from genres.serializers import GenreSerializer
 
-@csrf_exempt
-def genre_create_list_view(request):
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+  
+
+
+# @csrf_exempt
+# def genre_create_list_view(request):
     
-    if request.method == 'GET':
-        genres = Genre.objects.all()
-        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-        # data = []
-        # for genre in genres:
-        #     data.append(
-        #     {'id': genre.id, 'name': genre.name}
-        # )
-        return JsonResponse(data, safe=False)
-    elif request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        new_genre = Genre(name=data['name'])
-        new_genre.save()
-        return JsonResponse(
-            {'id': new_genre.id, 'name': new_genre.name},
-            status=201,
-        )
+#     if request.method == 'GET':
+#         genres = Genre.objects.all()
+#         data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+#         # data = []
+#         # for genre in genres:
+#         #     data.append(
+#         #     {'id': genre.id, 'name': genre.name}
+#         # )
+#         return JsonResponse(data, safe=False)
+#     elif request.method == 'POST':
+#         data = json.loads(request.body.decode('utf-8'))
+#         new_genre = Genre(name=data['name'])
+#         new_genre.save()
+#         return JsonResponse(
+#             {'id': new_genre.id, 'name': new_genre.name},
+#             status=201,
+#         )
 @csrf_exempt    
 def genre_detail_view(request, pk):
     genre = get_object_or_404(Genre,pk=pk)
