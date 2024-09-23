ae38ef9322d57d4825ebcb46ad7a1c17679ba24b
paulomorenodev <23208589@aluno.univesp.br>
2024-09-23 09:54:05 -0300

    model_serializer_validate_class_143

--- movies/serializer.py
+++ movies/serializer.py
@@ -5,3 +5,13 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError("A data de lançamento não pode ser anterior a 1990.")
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError("O resumo não deve ser mais de 200 caracteres.")
+        return value
