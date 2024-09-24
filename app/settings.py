7dd868da8555d1b25b0ed8ff933eaba975fd71d1
paulomorenodev <23208589@aluno.univesp.br>
2024-09-24 04:41:08 -0300

    jwt_install_class_150

--- app/settings.py
+++ app/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
 
     'genres',
     'actors',
     'movies',
     'reviews',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +137,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
