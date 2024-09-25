ac1a38abeabcbaf45e10afa5bc5fd9248b65f8e5
paulomorenodev <23208589@aluno.univesp.br>
2024-09-25 15:47:43 -0300

    implement_endpoint_statitisc_class_164

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
+from django.db.models import Count, Avg
 from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
 from movies.serializer import MovieSerializer
 from app.permissions import GlobalDefaultPermission
+from reviews.models import Review
 
 class MovieCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
@@ -19,7 +21,19 @@ class MovieStatsView(views.APIView):
     queryset = Movie.objects.all()
 
     def get(self, request):
+        total_movies = self.queryset.count()
+        #print(total_movies)
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_rewiews = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
+
         return response.Response(
-            data={'message': 'Paulo Moreno a caminho da fluencia em Python...Show!!!!!'},
+            data={
+                'total_movies': total_movies,
+                'movies_by_genre': movies_by_genre,
+                'total_reviews': total_rewiews,
+                'average_stars': round(average_stars,2) if average_stars else 0,
+            },
             status=status.HTTP_200_OK,
         )
