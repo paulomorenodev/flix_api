ce79058e2457d3c894f00cb429b48519c6304aa5
paulomorenodev <23208589@aluno.univesp.br>
2024-09-25 15:01:09 -0300

    create_endpoint_statitisc_class_163

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
 from movies.serializer import MovieSerializer
@@ -13,3 +13,13 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        return response.Response(
+            data={'message': 'Paulo Moreno a caminho da fluencia em Python...Show!!!!!'},
+            status=status.HTTP_200_OK,
+        )
